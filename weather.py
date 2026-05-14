fb99ac760e45a9550ac4e328f58c1d61617fc452
venukaranam98 <vk3344556677@gmail.com>
2026-05-14 12:18:44 +0530

    Weather app that fetches live data via Api

---
+++ weather.py
@@ -0,0 +1,44 @@
+import requests
+
+try:
+
+    city = input("Enter city name: ")
+
+    response = requests.get(
+        f"https://wttr.in/{city}?format=j1"
+    )
+
+    data = response.json()
+
+    print("\n--- WEATHER REPORT ---")
+
+    print(
+        "City:",
+        city
+    )
+
+    print(
+        "Temperature:",
+        data["current_condition"][0]["temp_C"],
+        "°C"
+    )
+
+    print(
+        "Weather:",
+        data["current_condition"][0]["weatherDesc"][0]["value"]
+    )
+
+    print(
+        "Humidity:",
+        data["current_condition"][0]["humidity"]
+    )
+
+    print(
+        "Wind Speed:",
+        data["current_condition"][0]["windspeedKmph"],
+        "km/h"
+    )
+
+except:
+
+    print("Weather data not found")
