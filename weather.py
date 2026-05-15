85c5ed66bb23cebf6df59f781bfb375d1add9780
venukaranam98 <vk3344556677@gmail.com>
2026-05-15 13:14:35 +0530

    Weather app via Api

--- weather.py
+++ weather.py
@@ -1,44 +1,132 @@
 import requests
+import logging
+import json
 
-try:
+logging.basicConfig(level=logging.INFO)
 
-    city = input("Enter city name: ")
 
-    response = requests.get(
-        f"https://wttr.in/{city}?format=j1"
-    )
+weather_history = []
 
-    data = response.json()
 
-    print("\n--- WEATHER REPORT ---")
+while True:
 
-    print(
-        "City:",
-        city
-    )
+    print("\n--- WEATHER APP ---")
 
-    print(
-        "Temperature:",
-        data["current_condition"][0]["temp_C"],
-        "°C"
-    )
+    print("1. Check Weather")
 
-    print(
-        "Weather:",
-        data["current_condition"][0]["weatherDesc"][0]["value"]
-    )
+    print("2. View Search History")
 
-    print(
-        "Humidity:",
-        data["current_condition"][0]["humidity"]
-    )
+    print("3. Save History")
 
-    print(
-        "Wind Speed : ",
-        data["current_condition"][0]["windspeedKmph"],
-        "km/h"
-    )
+    print("4. Exit")
 
-except:
 
-    print("Weather data not found")
+    choice = input("Enter choice: ")
+
+
+    if choice == "1":
+
+        try:
+
+            city = input("Enter city name: ")
+
+            response = requests.get(
+                f"https://wttr.in/{city}?format=j1"
+            )
+
+            if response.status_code == 200:
+
+                data = response.json()
+
+                logging.info(
+                    "Weather data fetched successfully"
+                )
+
+                weather = {
+                    "city": city,
+                    "temperature":
+                    data["current_condition"][0]["temp_C"],
+                    "condition":
+                    data["current_condition"][0]
+                    ["weatherDesc"][0]["value"],
+                    "humidity":
+                    data["current_condition"][0]["humidity"],
+                    "wind_speed":
+                    data["current_condition"][0]
+                    ["windspeedKmph"]
+                }
+
+                weather_history.append(weather)
+
+                print("\n--- WEATHER REPORT ---")
+
+                print("City:", weather["city"])
+
+                print(
+                    "Temperature:",
+                    weather["temperature"],
+                    "°C"
+                )
+
+                print(
+                    "Weather:",
+                    weather["condition"]
+                )
+
+                print(
+                    "Humidity:",
+                    weather["humidity"]
+                )
+
+                print(
+                    "Wind Speed:",
+                    weather["wind_speed"],
+                    "km/h"
+                )
+
+            else:
+
+                logging.warning(
+                    "API returned warning status"
+                )
+
+        except:
+
+            logging.error(
+                "Failed to fetch weather data"
+            )
+
+
+    elif choice == "2":
+
+        print("\n--- SEARCH HISTORY ---")
+
+        for item in weather_history:
+
+            print(
+                item["city"],
+                "-",
+                item["temperature"],
+                "°C"
+            )
+
+
+    elif choice == "3":
+
+        with open("weather_history.json", "w") as file:
+
+            json.dump(weather_history, file)
+
+        print("History saved")
+
+
+    elif choice == "4":
+
+        print("Program closed")
+
+        break
+
+
+    else:
+
+        print("Invalid choice")
